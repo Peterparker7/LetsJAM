9819443401a1ced0b76168a7fe5eb89c34e0032c
peter <peter840101@gmail.com>
2021-05-14 11:00:01 +0800

    sprint1 day1 day2

---
+++ src/Detail.js
@@ -0,0 +1,14 @@
+import "./App.css";
+// import styled from "styled-components";
+import React, { useEffect, useState } from "react";
+// import { useParams } from "react-router-dom";
+import { getActivityData } from "./utils/firebase";
+
+function Detail() {}
+
+// const getFirebaseData = async () => {
+//     const data = await getActivityData();
+//     setData(data);
+//   };
+
+export default Detail;
