57ce53e15ea082f4cf114641368325f8819121d8
peter <peter840101@gmail.com>
2021-06-21 21:41:23 +0800

    firebase apikey

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -5,12 +5,12 @@ import "firebase/storage";
 
 var firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  authDomain: "personalproject-33263.firebaseapp.com",
+  projectId: "personalproject-33263",
+  storageBucket: "personalproject-33263.appspot.com",
+  messagingSenderId: "966021952087",
+  appId: "1:966021952087:web:5c52cfb31b031cdf6a6912",
+  measurementId: "G-MXQWY9WWZK",
 };
 // var firebaseConfig = {
 //   // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
